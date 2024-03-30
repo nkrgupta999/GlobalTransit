c3bb224e10be316519673de774ae21c794c3c620
nkrgupta999 <nitinkumargupta999@outlook.com>
2024-03-30 15:49:38 +0530

    Modify Vehicle Category Frame and add Delete Button & Fixing the sizing of the frame

--- src/Gui/VehicleCategory.java
+++ src/Gui/VehicleCategory.java
@@ -1,4 +1,5 @@
 package Gui;
+
 import static java.awt.Frame.MAXIMIZED_BOTH;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -7,26 +8,40 @@ import java.sql.SQLIntegrityConstraintViolationException;
 import java.sql.SQLException;
 import java.sql.Statement;
 import javax.swing.JOptionPane;
+
 public class VehicleCategory extends javax.swing.JFrame {
+
     public VehicleCategory() {
         initComponents();
-        javax.swing.table.DefaultTableModel dtm =(javax.swing.table.DefaultTableModel) jTable1.getModel();
+        getEntries();
+    }
+
+    private void getEntries() {
+        javax.swing.table.DefaultTableModel dtm = (javax.swing.table.DefaultTableModel) jTable1.getModel();
+        // Remove privious data in Jtable and refresh everytime 
+        int rc = dtm.getRowCount();
+        for (int a = 0; a < rc; a++) {
+            dtm.removeRow(0);
+        }
+        // Add privious data in Jtablefrom Database and refresh everytime 
         try {
-            ResultSet rs= db.DbConnect.st.executeQuery("Select * from category");
-            int x=0;
-            while (rs.next()) {                
-                String n=rs.getString("name");
+            ResultSet rs = db.DbConnect.st.executeQuery("Select * from category");
+            int x = 0;
+            while (rs.next()) {
+                String n = rs.getString("name");
                 x++;
                 java.util.Vector r = new java.util.Vector();
                 r.add(x);
                 r.add(n);
                 dtm.addRow(r);
-                        
+
             }
-            
         } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, e);
         }
+
     }
+
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
@@ -36,13 +51,14 @@ public class VehicleCategory extends javax.swing.JFrame {
         jButton1 = new javax.swing.JButton();
         jScrollPane1 = new javax.swing.JScrollPane();
         jTable1 = new javax.swing.JTable();
+        jButton2 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Global Transit | Vehicle Category");
 
         jLabel1.setText("New Vehicle Category");
 
-        jButton1.setText("ADD");
+        jButton1.setText("Add");
         jButton1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jButton1ActionPerformed(evt);
@@ -59,6 +75,15 @@ public class VehicleCategory extends javax.swing.JFrame {
         ));
         jScrollPane1.setViewportView(jTable1);
 
+        jButton2.setBackground(new java.awt.Color(255, 0, 51));
+        jButton2.setForeground(new java.awt.Color(255, 255, 255));
+        jButton2.setText("Delete");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -69,9 +94,15 @@ public class VehicleCategory extends javax.swing.JFrame {
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(n, javax.swing.GroupLayout.PREFERRED_SIZE, 193, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jButton1, javax.swing.GroupLayout.DEFAULT_SIZE, 163, Short.MAX_VALUE))
+                .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 126, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 140, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
             .addComponent(jScrollPane1)
         );
+
+        layout.linkSize(javax.swing.SwingConstants.HORIZONTAL, new java.awt.Component[] {jButton1, jButton2});
+
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
@@ -79,12 +110,13 @@ public class VehicleCategory extends javax.swing.JFrame {
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 33, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(n, javax.swing.GroupLayout.PREFERRED_SIZE, 33, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jButton1))
+                    .addComponent(jButton1)
+                    .addComponent(jButton2))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 275, Short.MAX_VALUE))
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 290, Short.MAX_VALUE))
         );
 
-        layout.linkSize(javax.swing.SwingConstants.VERTICAL, new java.awt.Component[] {jButton1, jLabel1, n});
+        layout.linkSize(javax.swing.SwingConstants.VERTICAL, new java.awt.Component[] {jButton1, jButton2, jLabel1, n});
 
         pack();
         setLocationRelativeTo(null);
@@ -92,17 +124,35 @@ public class VehicleCategory extends javax.swing.JFrame {
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         // ----------ADD NEW CATEGORY BUTTON CODE----------
-        try{
-        
-    String name=n.getText();
-        db.DbConnect.st.executeUpdate("insert into category values('"+name+"')");
-        JOptionPane.showMessageDialog(null,"Vehicle Added Successfully");
-    }catch(SQLIntegrityConstraintViolationException ex){
-        JOptionPane.showMessageDialog(null, "Vehicle Already Exists \n Use Another Type of Vechile");
-    }catch(Exception ex){
-        JOptionPane.showMessageDialog(null, ex);
-    }
+        try {
+
+            String name = n.getText();
+            db.DbConnect.st.executeUpdate("insert into category values('" + name + "')");
+            JOptionPane.showMessageDialog(null, "Vehicle Added Successfully");
+            getEntries();
+        } catch (SQLIntegrityConstraintViolationException ex) {
+            JOptionPane.showMessageDialog(null, "Vehicle Already Exists \n Use Another Type of Vechile");
+        } catch (Exception ex) {
+            JOptionPane.showMessageDialog(null, ex);
+        }
     }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        // TODO add your handling code here:
+        try {
+            int ri=jTable1.getSelectedRow();
+            if(ri==-1){
+                JOptionPane.showMessageDialog(null, "Please Select Vehicle !");
+            }else{
+            String n=(String)jTable1.getValueAt(ri,1);
+            db.DbConnect.st.executeUpdate("delete from category where name='" + n + "'");
+            JOptionPane.showMessageDialog(null, "Vehicle Delete Successfully");
+            getEntries();
+            }
+        } catch (Exception ex) {
+            JOptionPane.showMessageDialog(null, ex);
+        }
+    }//GEN-LAST:event_jButton2ActionPerformed
 
     public static void main(String args[]) {
         java.awt.EventQueue.invokeLater(new Runnable() {
@@ -114,6 +164,7 @@ public class VehicleCategory extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTable jTable1;
